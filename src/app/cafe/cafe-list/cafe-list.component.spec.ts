8a2b33da94ae269eb49140593706aa3e89811b20
jcamilorg <juancamilorg64@gmail.com>
2022-11-04 17:25:50 -0500

    se agregan metodos de prueba

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -45,7 +45,60 @@ describe('CafeListComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have a tr inside the thead element', () => {
+    expect(debug.queryAll(By.css('thead tr'))).toHaveSize(1);
+  });
+
+  it('should have a th in thead with text # Nombre Tipo Region', () => {
+    const titles = ['#', 'Nombre', 'Tipo', 'Region'];
+
+    const childre = debug.query(By.css('thead tr')).children;
+    childre.forEach((th, i) => {
+      expect(th.nativeElement.textContent).toContain(titles[i]);
+    });
+  });
+
   it('should create 3 tr elemenst corresponding to the three coffies', () => {
-    expect(debug.queryAll(By.css('tr'))).toHaveSize(3);
+    const bodyTable = debug.query(By.css('tbody'));
+    expect(bodyTable.children).toHaveSize(3);
+  });
+
+  it('should th element content the id', () => {
+    debug.queryAll(By.css('th[scope="row"]')).forEach((th, i) => {
+      expect(th.nativeElement.textContent).toContain(component.cafes[i].id);
+    });
+  });
+
+  it('should th in tbody contain the id, nombre, Tipo, Region of a coffie', () => {
+    debug.queryAll(By.css('tbody tr')).forEach((tr, i) => {
+      const children = tr.children;
+      expect(children[0].nativeElement.textContent).toContain(
+        component.cafes[i].id
+      );
+      expect(children[1].nativeElement.textContent).toContain(
+        component.cafes[i].nombre
+      );
+      expect(children[2].nativeElement.textContent).toContain(
+        component.cafes[i].tipo
+      );
+      expect(children[3].nativeElement.textContent).toContain(
+        component.cafes[i].region
+      );
+    });
+  });
+
+  it('should have 2 p elements with the variables CafeOrigen and cafe Blend', () => {
+    const ptags = debug.queryAll(By.css('p'));
+
+    expect(ptags[0].nativeElement.textContent).toContain(
+      `Total café de origen: ${component.cafeOrigen}`
+    );
+    expect(ptags[1].nativeElement.textContent).toContain(
+      `Total café blend: ${component.cafeBlend}`
+    );
+  });
+
+  it('should have img element', () => {
+    expect(debug.queryAll(By.css('img'))).toHaveSize(1);
   });
 });
