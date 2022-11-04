b8dae69cdc8cdaec2751d2833d6d89899161234a
jcamilorg <juancamilorg64@gmail.com>
2022-11-04 14:29:44 -0500

    Creacion del servicio para listar cafes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { CafeModule } from './cafe/cafe.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CafeModule],
+  imports: [BrowserModule, AppRoutingModule, CafeModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
 
 @Component({
   selector: 'app-cafe-list',
   templateUrl: './cafe-list.component.html',
-  styleUrls: ['./cafe-list.component.css']
+  styleUrls: ['./cafe-list.component.css'],
 })
 export class CafeListComponent implements OnInit {
+  cafes: Array<Cafe> = [];
 
-  constructor() { }
+  constructor(private cafeService: CafeService) {}
 
   ngOnInit() {
+    this.getCafes();
   }
 
+  getCafes(): void {
+    this.cafeService.getCafes().subscribe((cafes) => {
+      console.log(cafes);
+      this.cafes = cafes;
+    });
+  }
 }
